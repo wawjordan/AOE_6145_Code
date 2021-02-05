13f59a4417f275d5ad06cde066b766d441f6eec9
wawjordan <wajordan@vt.edu>
2021-02-05 10:13:25 -0500

    Quasi-1D nozzle module created

---
+++ HW1/Q1D.f95
@@ -0,0 +1,24 @@
+!=================================== Q1D =====================================80
+module Q1D
+
+  use set_precision, only : prec
+  use set_constants, only : zero, one, two, half
+  use set_inputs,    only : g, gp1, gm1, R
+  use subroutines,   only : newton_safe
+  implicit none
+
+  private
+
+  public M, rho, u, p, T
+
+  integer    :: k
+  integer    :: N
+  real(prec), dimension (:), allocatable :: M
+  real(prec), dimension (:), allocatable :: psi
+  real(prec), dimension (:), allocatable :: rho
+  real(prec), dimension (:), allocatable :: u
+  real(prec), dimension (:), allocatable :: p
+  real(prec), dimension (:), allocatable :: T
+
+
+end module Q1D
